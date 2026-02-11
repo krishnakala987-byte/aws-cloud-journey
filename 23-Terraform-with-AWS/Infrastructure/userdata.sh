71f01d3e31b89a04dea62c14bdeef402b91da259
krishnakala987-byte <krishnakala987@gmail.com>
2026-02-11 10:00:34 +0530

    Add userdata script for Apache installation and setup
    This script updates the system, installs Apache, and creates a simple HTML page displaying the instance ID and a message.

---
+++ 23-Terraform-with-AWS/Infrastructure/userdata.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt update -y
+apt install -y apache2
+
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-family: Arial;
+      background-color: #f4f4f4;
+      text-align: center;
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+  </style>
+</head>
+<body>
+  <h1>Terraform Project Server</h1>
+  <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
+  <p>Infrastructure deployed using Terraform</p>
+</body>
+</html>
+EOF
+
+systemctl start apache2
+systemctl enable apache2
