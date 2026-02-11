514e6989177fcf8b4305a476240c251165779171
krishnakala987-byte <krishnakala987@gmail.com>
2026-02-11 09:52:13 +0530

    Refactor AWS resources for improved naming and structure

--- 23-Terraform-with-AWS/Infrastructure/main.tf
+++ 23-Terraform-with-AWS/Infrastructure/main.tf
@@ -1,170 +1,138 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
-
-  tags = {
-    Name = "project-vpc"
-  }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "sub2" {
+  vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-2"
-  }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "project-igw"
-  }
+  vpc_id = aws_vpc.myvpc.id
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "websg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
 
   tags = {
-    Name = "public-rt"
+    Name = "websg"
   }
 }
 
-resource "aws_route_table_association" "subnet1_assoc" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "subnet2_assoc" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_security_group" "web_sg" {
-  name   = "web-sg"
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "web-sg"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "http" {
-  security_group_id = aws_security_group.web_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.websg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh" {
-  security_group_id = aws_security_group.web_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.websg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "all" {
-  security_group_id = aws_security_group.web_sg.id
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.websg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
-resource "aws_instance" "web1" {
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "example-project-bucket"
+}
+
+resource "aws_instance" "webserver1" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id              = aws_subnet.sub1.id
   user_data              = file("userdata.sh")
-
-  tags = {
-    Name = "web-server-1"
-  }
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "webserver2" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet2.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id              = aws_subnet.sub2.id
   user_data              = file("userdata1.sh")
-
-  tags = {
-    Name = "web-server-2"
-  }
 }
 
-resource "aws_lb" "alb" {
-  name               = "project-alb"
+resource "aws_lb" "myalb" {
+  name               = "myalb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-
-  tags = {
-    Name = "project-alb"
-  }
+  security_groups    = [aws_security_group.websg.id]
+  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "project-tg"
+  name     = "mytg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = aws_vpc.myvpc.id
 
   health_check {
     path = "/"
     port = "traffic-port"
   }
-
-  tags = {
-    Name = "project-tg"
-  }
 }
 
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.web1.id
+  target_id        = aws_instance.webserver1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.web2.id
+  target_id        = aws_instance.webserver2.id
   port             = 80
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
+  load_balancer_arn = aws_lb.myalb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
   }
 }
 
-output "load_balancer_dns" {
-  value = aws_lb.alb.dns_name
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
 }
