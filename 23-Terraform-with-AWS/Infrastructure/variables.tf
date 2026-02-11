9c8035628333f309c5c98cc896bdf92d287b84f3
krishnakala987-byte <krishnakala987@gmail.com>
2026-02-11 09:54:49 +0530

    Remove unused variables from variables.tf
    Removed unused variables for region and ami_id from variables.tf.

--- 23-Terraform-with-AWS/Infrastructure/variables.tf
+++ 23-Terraform-with-AWS/Infrastructure/variables.tf
@@ -1,14 +1,4 @@
-variable "region" {
-  description = "AWS region where resources will be created"
-  default     = "us-east-1"
-}
-
 variable "cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  default     = "ami-xxxxxxxx"
 }
